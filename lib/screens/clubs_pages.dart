62ff86c3bb17c4aae3375ba7f1f4641766700df9
Adem HADHRAOUI <ademmhadhraoui@gmail.com>
2026-02-19 17:23:25 +0100

    update in 29/01/2026

--- lib/screens/clubs_pages.dart
+++ lib/screens/clubs_pages.dart
@@ -1,34 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:enstabhouse/screens/home_feed_screen.dart';
 class ClubsPages extends StatelessWidget {
   const ClubsPages({super.key});
   static final List<EventWidget> event = [
       EventWidget(
-          Name: 'BootCamp',
-          Description: "Flutter BootCamp for enstab students",
-          Date: "18 Feb",
-          Place: "Enstab",
-          Time: "12 pm" ,
+          name: 'BootCamp',
+          description: "Flutter BootCamp for enstab students",
+          date: "18 Feb",
+          place: "Enstab",
+          time: "12 pm" ,
       ),
     EventWidget(
-      Name: 'BootCamp',
-      Description: "Flutter BootCamp for enstab students",
-      Date: "18 Feb",
-      Place: "Enstab",
-      Time: "12 pm" ,
+      name: 'BootCamp',
+      description: "Flutter BootCamp for enstab students",
+      date: "18 Feb",
+      place: "Enstab",
+      time: "12 pm" ,
     ),
     EventWidget(
-      Name: 'BootCamp',
-      Description: "Flutter BootCamp for enstab students",
-      Date: "18 Feb",
-      Place: "Enstab",
-      Time: "12 pm" ,
+      name: 'BootCamp',
+      description: "Flutter BootCamp for enstab students",
+      date: "18 Feb",
+      place: "Enstab",
+      time: "12 pm" ,
     ),
     EventWidget(
-      Name: 'BootCamp',
-      Description: "Flutter BootCamp for enstab students",
-      Date: "18 Feb",
-      Place: "Enstab",
-      Time: "12 pm" ,
+      name: 'BootCamp',
+      description: "Flutter BootCamp for enstab students",
+      date: "18 Feb",
+      place: "Enstab",
+      time: "12 pm" ,
     ),
 
   ] ;
@@ -40,6 +41,54 @@ class ClubsPages extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
+              Container(
+                height: 150,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Color(0xFF9E0815) ,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20 , top: 20),
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          GestureDetector(
+                            onTap: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=> HomeFeedScreen())
+                              );
+                            },
+                            child: Icon(Icons.arrow_back ,
+                            color: Colors.white,
+                              size: 35,
+                            ),
+                          ),
+                          SizedBox(width: 20,) ,
+                          Text("Photography Club",
+                            style: TextStyle(
+                              color: Colors.white ,
+                              fontSize: 23
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 30,) ,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(height: 20,),
+                          SelectedChipWidget(text: "Feed", selected: false, ) ,
+                          SizedBox(width: 25,),
+                          SelectedChipWidget(text: "Events", selected: true) ,
+                          SizedBox(width: 25,),
+                          SelectedChipWidget(text: "Workshops", selected: false, ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
              Expanded(
                child: ListView.builder(
                  padding: const EdgeInsets.only(top: 10),
@@ -57,18 +106,18 @@ class ClubsPages extends StatelessWidget {
   }
 }
 class EventWidget {
-  final String Name ;
-  final String Description ;
-  final String Date ;
-  final String Place ;
-  final String Time ;
+  final String name ;
+  final String description ;
+  final String date ;
+  final String place ;
+  final String time ;
 
   EventWidget({
-    required this.Name ,
-    required this.Description ,
-    required this.Date ,
-    required this.Place ,
-    required this.Time ,
+    required this.name ,
+    required this.description ,
+    required this.date ,
+    required this.place ,
+    required this.time ,
 }) ;
 }
 class EventWidgetCard extends StatelessWidget {
@@ -93,34 +142,122 @@ class EventWidgetCard extends StatelessWidget {
       ),
       child: Row(
         children: [
-          Container(
-            height: 220,
-            width: 100,
-            padding: EdgeInsets.symmetric(horizontal: 15 ,vertical: 10),
-            decoration: BoxDecoration(
-              color: Color(0xFF9E0815) , 
-              borderRadius: BorderRadius.circular(16)
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0) ,
+            child: Container(
+              height: 220,
+              width: 100,
+              padding: EdgeInsets.symmetric(horizontal: 15 ,vertical: 10),
+              decoration: BoxDecoration(
+                color: Color(0xFF9E0815) ,
+                borderRadius: BorderRadius.circular(16)
+              ),
+              child:Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(eventwidget.date ,
+                    maxLines: 2,
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: Colors.white ,
+                    fontWeight: FontWeight.bold
+                  ),
+                  ),
+                  const SizedBox(height: 20.0,) ,
+                  Icon(Icons.calendar_month,
+                  color: Colors.white,
+                  ),
+                ],
+              ),
             ),
-            child:Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(eventwidget.Date ,
-                style: TextStyle(
-                  color: Colors.white ,
-                  fontWeight: FontWeight.bold
-                ),
-                ),
-                const SizedBox(height: 20.0,) ,
-                Icon(Icons.calendar_month,
-                color: Colors.white,
-                ),
-              ],
-            ),
-
           ),
+          SizedBox(width: 12.0,) ,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(eventwidget.name ,
+                  style: TextStyle(
+                    fontSize: 25,
+                      fontWeight: FontWeight.bold ,
+                      color: Colors.black
+                  ),
+                  ),
+                  Text(eventwidget.time ,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey ,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.place_outlined),
+                      Text(eventwidget.place ,
+                        softWrap: true ,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.grey ,
+                        ),
+                      ),
+
+                    ],
+                  ),
+                  SizedBox(height: 10,) ,
+                  Text(eventwidget.description,
+                    softWrap: true ,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey ,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
         ],
       )
     );
   }
 }
+class SelectedChipWidget extends StatelessWidget {
+  final String text ;
+  final bool selected ;
+
+
+  SelectedChipWidget ({
+    required this.text ,
+    required this.selected ,
+
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap:(){
+        } ,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          color: selected ? Color(0xFF7d1616) : Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: selected ? Colors.white : Colors.black,
+          ),
+        ),
+      ),
+    ) ;
+  }
+}
+
 
