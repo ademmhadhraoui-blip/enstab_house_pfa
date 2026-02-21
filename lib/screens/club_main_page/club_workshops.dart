a7e097ba03aab16878bef05d0e59cc1862cdd808
Adem HADHRAOUI <ademmhadhraoui@gmail.com>
2026-02-21 02:47:53 +0100

    update in 29/01/2026

--- lib/screens/club_main_page/club_workshops.dart
+++ lib/screens/club_main_page/club_workshops.dart
@@ -3,24 +3,81 @@ import 'club_events.dart';
 
 class ClubWorkshops extends StatelessWidget {
   const ClubWorkshops({super.key});
+  static List<WorkShopWidget> workshop = [
+    WorkShopWidget(time: "Feb 8 , 12 pm",
+        description:"welcome to AI Bootcamp where you will learn the fondamentals of ai with the best instructor " ,
+        place: "Enstab",
+        name: "AI BOOTCAMP" ,
+        instructor: "Mr Mohamed",
+    ) ,
+    WorkShopWidget(time: "Feb 8 , 12 pm",
+      description:"welcome to AI Bootcamp where you will learn the fondamentals of ai with the best instructor " ,
+      place: "Enstab",
+      name: "AI BOOTCAMP" ,
+      instructor: "Mr Mohamed",
+    ) ,
+    WorkShopWidget(time: "Feb 8 , 12 pm",
+      description:"welcome to AI Bootcamp where you will learn the fondamentals of ai with the best instructor " ,
+      place: "Enstab",
+      name: "AI BOOTCAMP" ,
+      instructor: "Mr Mohamed",
+    ) ,
+    WorkShopWidget(time: "Feb 8 , 12 pm",
+      description:"welcome to AI Bootcamp where you will learn the fondamentals of ai with the best instructor " ,
+      place: "Enstab",
+      name: "AI BOOTCAMP" ,
+      instructor: "Mr Mohamed",
+    ) ,
+    
+  ];
 
   @override
   Widget build(BuildContext context) {
     return  SafeArea(
       child: Scaffold(
         backgroundColor: Colors.grey[100],
-        body:  WorkshopCard(),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.only(top: 10),
+                itemCount: workshop.length,
+                itemBuilder: (context ,index){
+                  return WorkshopCard(workshopwidget  : workshop[index]) ;
+                },
+              ),
+            )
+          ]
+        )  ,
       ),
     ) ;
   }
 }
+class WorkShopWidget {
+  final String name ;
+  final String description ;
+  final String time ;
+  final String place ;
+  final String instructor ;
+  WorkShopWidget({
+    required this.time ,
+    required this.description ,
+    required this.place ,
+    required this.name ,
+    required this.instructor,
+  }) ;
+}
 class WorkshopCard extends StatelessWidget {
+  final WorkShopWidget workshopwidget ;
+  const WorkshopCard({
+    super.key,
+    required this.workshopwidget,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 200,
       margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
       decoration: BoxDecoration(
           color: Colors.white ,
@@ -68,8 +125,52 @@ class WorkshopCard extends StatelessWidget {
                 Icon(Icons.calendar_month ,
                 color: Colors.grey,
                 ),
+                SizedBox(width: 15,) ,
+                Text("Feb 8 * 12 am " ,
+                style: TextStyle(
+                  color: Colors.grey
+                ) ,
+                ) ,
               ],
-            )
+            ),
+            SizedBox(height: 10,),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Expanded(
+                child: Text("welcome to AI Bootcamp where you will learn the fondamentals of ai with the best instructor " ,
+                  softWrap: true,
+                  style: TextStyle(
+                    color: Colors.grey ,
+                  ),
+                ),
+              ),
+            ) ,
+            SizedBox(height: 10,) ,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  child: Container(
+                    width: 250,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF9E0815),
+                      borderRadius: BorderRadius.circular(10) ,
+                    ),
+                    child: Row(
+                      mainAxisAlignment:  MainAxisAlignment.center,
+                      children: [
+                        Text("Register",
+                        style: TextStyle(color: Colors.white,
+                        ),
+                        ),
+                      ],
+                    ),
+                  )
+                ),
+                SizedBox(height: 100,)
+              ],
+            ),
           ],
         ),
       ),
